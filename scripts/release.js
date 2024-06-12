aec297a53707d23edec543b636e626707350f9a2
sskyy <skyking_H@hotmail.com>
2024-06-12 21:37:05 +0900

    chore: remove build from release script

--- scripts/release.js
+++ scripts/release.js
@@ -16,29 +16,6 @@ if (!isClean) {
 }
 
 try {
-  // build 时间太长了，而且看不到反馈，所以注释掉，需要手动 build
-  // execSync('npm install')
-  // execSync('npm run build')
-  // 读取 package.json 内容
-  const packageJson =  require('../package.json')
-  // 从 icons.json 中为每一个 icon 重新生成 exports 字段。
-  const newExports = {}
-  const icons = require('../icons.json', {type: 'json'})
-  icons.forEach(icon => {
-    // 连字符转成驼峰
-    const iconName = icon.name.replace(/-([a-z])/g, g => g[1].toUpperCase())
-    // 首字母也要大写
-    const exportName = iconName.charAt(0).toUpperCase() + iconName.slice(1)
-    newExports[`./${exportName}.js`] = `./dist/${exportName}.js`
-  })
-  // 重新写入 package.json
-  packageJson.exports = newExports
-  fs.writeFileSync('./package.json', `${JSON.stringify(packageJson, null, 2)}\n`)
-
-  execSync('git add .')
-  execSync('git commit -m "release-prepare: update package.json"')
-
-
   const newVersion = execSync(`npm version ${version}`)
   execSync('git push')
   execSync(`npm publish ./`)
